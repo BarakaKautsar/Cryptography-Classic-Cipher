e78f538175c31f7c38a696488054bd06d20f9673
BarakaKautsar <36478251+BarakaKautsar@users.noreply.github.com>
2023-02-03 08:05:28 +0700

    gui vigenere

---
+++ gui/sampleApp.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from tkinter import *
+import gui
+
+class SampleApp(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self._frame = None
+        self.switch_frame(gui.Landing)
+
+    def switch_frame(self, frame_class):
+        """Destroys current frame and replaces it with a new one."""
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.pack()
+
+if __name__ == "__main__":
+    app = SampleApp()
+    app.mainloop()
